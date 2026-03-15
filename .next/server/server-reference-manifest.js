8e25fc0fd644c1f093a34379ec2c9240ee06e62b
sovereigneng-boop <sovereigneng@gmail.com>
2026-03-15 17:10:09 +0900

    Vercel Blob 빌드 오류 수정

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JHydDIyNj8D7PKVdOkoF/93FP/BcSQ8dQLi+oQqDjIc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"IgopSttjbcsJkDnJNyLW4O+o22ZRRWAMGbQBrUpotVQ=\"}"
